855cede408034b8841ab086f64260942d5f62f8f
ernestas13 <ternestas.trakys@gmail.com>
2024-09-17 16:11:12 +0100

    ee

--- site/bat/MailHandler.php
+++ site/bat/MailHandler.php
@@ -30,13 +30,24 @@
 		die('Attention, contact form will not work locally! Please upload your template to a live hosting server.');
 	}
 
-	if(isset($_POST['name']) and $_POST['name'] != ''){$message_body .= '<p>Visitor: ' . $_POST['name'] . '</p>' . "\n" . '<br>' . "\n"; $subject.=$_POST['name'];}
-	if(isset($_POST['email']) and $_POST['email'] != ''){$message_body .= '<p>Email Address: ' . $_POST['email'] . '</p>' . "\n" . '<br>' . "\n"; $user_email=$_POST['email'];}
-	if(isset($_POST['state']) and $_POST['state'] != ''){$message_body .= '<p>State: ' . $_POST['state'] . '</p>' . "\n" . '<br>' . "\n";}
-	if(isset($_POST['phone']) and $_POST['phone'] != ''){$message_body .= '<p>Phone Number: ' . $_POST['phone'] . '</p>' . "\n" . '<br>' . "\n";}	
-	if(isset($_POST['fax']) and $_POST['fax'] != ''){$message_body .= '<p>Fax Number: ' . $_POST['fax'] . '</p>' . "\n" . '<br>' . "\n";}
-	if(isset($_POST['message']) and $_POST['message'] != ''){$message_body .= '<p>Message: ' . $_POST['message'] . '</p>' . "\n";}	
+
+	if(isset($_POST['name']) and $_POST['name'] != ''){$message_body .= 'Visitor: ' . $_POST['name'] . '' . "\n" . '' . "\n"; $subject.=$_POST['name'];}
+	if(isset($_POST['email']) and $_POST['email'] != ''){$message_body .= 'Email Address: ' . $_POST['email'] . '' . "\n" . '' . "\n"; $user_email=$_POST['email'];}
+	if(isset($_POST['state']) and $_POST['state'] != ''){$message_body .= 'State: ' . $_POST['state'] . '' . "\n" . '' . "\n";}
+	if(isset($_POST['phone']) and $_POST['phone'] != ''){$message_body .= 'Phone Number: ' . $_POST['phone'] . '' . "\n" . '' . "\n";}	
+	if(isset($_POST['fax']) and $_POST['fax'] != ''){$message_body .= 'Fax Number: ' . $_POST['fax'] . '' . "\n" . '' . "\n";}
+	if(isset($_POST['message']) and $_POST['message'] != ''){$message_body .= 'Message: ' . $_POST['message'] . '' . "\n";}	
 	if(isset($_POST['stripHTML']) and $_POST['stripHTML']=='true'){$message_body = strip_tags($message_body);$message_type='text';}
+
+
+
+	// if(isset($_POST['name']) and $_POST['name'] != ''){$message_body .= '<p>Visitor: ' . $_POST['name'] . '</p>' . "\n" . '<br>' . "\n"; $subject.=$_POST['name'];}
+	// if(isset($_POST['email']) and $_POST['email'] != ''){$message_body .= '<p>Email Address: ' . $_POST['email'] . '</p>' . "\n" . '<br>' . "\n"; $user_email=$_POST['email'];}
+	// if(isset($_POST['state']) and $_POST['state'] != ''){$message_body .= '<p>State: ' . $_POST['state'] . '</p>' . "\n" . '<br>' . "\n";}
+	// if(isset($_POST['phone']) and $_POST['phone'] != ''){$message_body .= '<p>Phone Number: ' . $_POST['phone'] . '</p>' . "\n" . '<br>' . "\n";}	
+	// if(isset($_POST['fax']) and $_POST['fax'] != ''){$message_body .= '<p>Fax Number: ' . $_POST['fax'] . '</p>' . "\n" . '<br>' . "\n";}
+	// if(isset($_POST['message']) and $_POST['message'] != ''){$message_body .= '<p>Message: ' . $_POST['message'] . '</p>' . "\n";}	
+	// if(isset($_POST['stripHTML']) and $_POST['stripHTML']=='true'){$message_body = strip_tags($message_body);$message_type='text';}
 
 try{
 	include "libmail.php";
